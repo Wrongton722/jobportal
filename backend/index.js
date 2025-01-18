8cebf8da013040304d46508bb19bcc714e5fd64f
Wrongton722 <rupeshkr058@gmail.com>
2025-01-18 14:18:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,11 +20,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:process.env.CORS,
-    credentials:true
-}
+    origin: (origin, callback) => {
+        callback(null, true);
+    },
+    credentials: true
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 // app.use(express.static(path.join(__dirname,"/frontend/dist")));
 // app.get("*",(_,res)=>{
 //     res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
