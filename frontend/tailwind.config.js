dd3d86e9ac3c9080b2ecfbc0b428e9751dfad003
Wrongton722 <rupeshkr058@gmail.com>
2025-01-18 14:59:19 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
+
 export const darkMode = ["class"];
 export const content = [
   './pages/**/*.{js,jsx}',
@@ -72,4 +74,4 @@ export const theme = {
     },
   },
 };
-export const plugins = [require("tailwindcss-animate")];
+export const plugins = [tailwindcssAnimate];
