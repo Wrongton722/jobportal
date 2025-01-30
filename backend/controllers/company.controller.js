30ba1dc8a2321ef9ebd662513b37aa11220e8a81
Wrongton722 <rupeshkr058@gmail.com>
2025-01-30 20:25:35 +0530

    Update company.controller.js

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -72,14 +72,16 @@ export const getCompanyById = async (req, res) => {
 export const updateCompany = async (req, res) => {
     try {
         const { name, description, website, location } = req.body;
- 
-        const file = req.file;
-        // idhar cloudinary ayega
-        const fileUri = getDataUri(file);
-        const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
-        const logo = cloudResponse.secure_url;
-    
-        const updateData = { name, description, website, location, logo };
+        if(req.file){
+            const file = req.file;
+            const fileUri = getDataUri(file);
+            const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
+            logo = cloudResponse.secure_url;
+        }
+        const updateData = { name, description, website, location};
+        if(logo){
+          updateData.logo = logo;  
+        } 
 
         const company = await Company.findByIdAndUpdate(req.params.id, updateData, { new: true });
 
